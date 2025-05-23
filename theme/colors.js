7109422b76874556df4e7575cc4954591600c58c
Byamasu Patrick Paul <43136469+byamasu-patrick@users.noreply.github.com>
2025-05-23 21:43:51 +0200

    Change color and add new alooo logo

--- theme/colors.js
+++ theme/colors.js
@@ -391,7 +391,7 @@ export const colors = {
     },
 
     black: '#000000',
-    brand: '#2781F6',
+    brand: '#5FC95B',
     background: 'rgb(var(--background-color) / <alpha-value>)',
     solid: {
       1: 'rgb(var(--solid-1) / <alpha-value>)',
